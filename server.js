be48b58879f99850af6801496ccaea21c8ad8b69
Nathan Mayhook <jnmayhook@gmail.com>
2022-06-21 23:36:12 -0700

    routes, fs, etc.

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const path = require('path');
+const notes = require('./db/db.json');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true })); //
+
+app.use(express.static('public')); // makes visible to everyone
+
+app.get('/', (req, res) => { // created home page
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => { // created notes page
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    notes.push(req.body);
+    // write note into the file
+    
+    // Convert the data to a string so we can save it
+    const noteString = JSON.stringify(notes);
+
+    // Write the string to a file
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Notes has been written to JSON file`
+          )
+    );
+    console.log(notes);
+    res.status(200).json(notes); 
+});
+
+
+app.listen(PORT, () => // activates the port
+    console.log(`App listenting at http://localhost:${PORT}`)
+); 
+
+
+
+
+
+
+
+
+// On the back end, the application should include a db.json file that will be used to store and retrieve notes using the fs module.
+
+// The following HTML routes should be created:
+
+// GET /notes should return the notes.html file.
+
+// GET * should return the index.html file.
+
+// The following API routes should be created:
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+
+// GIVEN a note-taking application
+// WHEN I open the Note Taker
+// THEN I am presented with a landing page with a link to a notes page
+// WHEN I click on the link to the notes page
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+// WHEN I enter a new note title and the note’s text
+// THEN a Save icon appears in the navigation at the top of the page
+// WHEN I click on the Save icon
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+// WHEN I click on an existing note in the list in the left-hand column
+// THEN that note appears in the right-hand column
+// WHEN I click on the Write icon in the navigation at the top of the page
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
